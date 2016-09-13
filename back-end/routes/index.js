1b97ccd784915723032e204311b53bab841a0bfc
ericettensohn <ericettensohn@gmail.com>
2016-09-13 16:57:54 -0400

    Login/Registration
    Dropdowns completed and styled, backend completed

--- back-end/routes/index.js
+++ back-end/routes/index.js
@@ -4,6 +4,7 @@ var mongoose = require('mongoose');
 var mongoUrl = "mongodb://localhost:27017/ecommerce";
 var Account = require('../models/account');
 mongoose.connect(mongoUrl);
+var bcrypt = require('bcrypt-nodejs');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -14,15 +15,41 @@ router.post('/addAccount', function(req, res, next){
 	
 	var accountToAdd = new Account({
 		username: req.body.username,
-		password: req.body.password,
+		password: bcrypt.hashSync(req.body.password),
 		email: req.body.email
 	});
 
 	accountToAdd.save();
 	res.json({
-		message: "\u2713 added",
-		username: username
+		message: "added",
+		username: req.body.username
 	});
 });
+
+router.post('/login', function(req, res, next){
+	Account.findOne({
+		username: req.body.username,	
+	}, function(error, document){
+		if(document == null) {
+			// no match
+			res.json({failure: "badUser"});
+		}
+		else {
+			// run comparesync, first param is english pass, second is the
+			var loginResult = bcrypt.compareSync(req.body.password, document.password);
+			if(loginResult){
+				// the password is correct
+				res.json({
+					success: "goodPass"
+				});
+			}
+			else {
+				res.json({
+					failure: "badPass"
+				});
+			}
+		}
+	})
+})
 
 module.exports = router;
--- front-end/scripts/mainController.js
+++ front-end/scripts/mainController.js
@@ -1,6 +1,6 @@
 var ecommerceApp = angular.module('ecommerceApp', ['ngRoute', 'ngCookies']);
 ecommerceApp.controller('mainController', function($scope, $http, $location, $cookies){
-	
+
 	var apiPath = "http://localhost:3000";
 
 	$scope.register = function(){
@@ -10,15 +10,43 @@ ecommerceApp.controller('mainController', function($scope, $http, $location, $co
 			password2: $scope.password2,
 			email: $scope.email
 		}).then(function successCallback(response){
-			console.log(response.data.message + ' ' + response.data.username);
+			console.log('\u2713' + response.data.message + ' ' + response.data.username);
 			if(response.data.message == 'added') {
-				$location.path('/options');
+				// $location.path('/options');
+				$scope.regSuccessful = true;
 			}
 		}, function errorCallback(response){
 			console.log(response)
 		});
 	}
+
+	$scope.login = function(){
+
+		$http.post(apiPath + '/login', {
+			username: $scope.username,
+			password: $scope.password
+		}).then(function successCallback(response){
+
+
+			if (response.data.failure == 'badUser'){
+				$scope.badUser = true;
+			}
+			else if (response.data.failure == 'badPass'){
+				$scope.badPass = true;
+			}
+			else if (response.data.success == 'goodPass'){
+				
+			}
+
+		}, function errorCallback(response){
+			console.log(response.data.failure)
+			
+		});
+	}
+
 });
+
+
 
 ecommerceApp.config(function($routeProvider){
 	$routeProvider.when('/', {
